266bd8cb502485f87c693b5a483b44f7082fc875
Abdallah Al Sawaqi <buzz644@yahoo.com>
2025-06-21 13:43:13 +0400

    testing

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Auth\LoginController;
  
 
 Route::get('/', function () {
-     echo "Welcome to the Laravel Applicationss!";
+     echo "Welcome to the Laravel Applications!";
 });
 
  
